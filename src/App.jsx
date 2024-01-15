abb038b57bd029e3de928df657da1449dbe44e72
theo <edem.teo@gmail.com>
2024-01-15 20:46:12 +0300

    to props.children

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Card } from "./components/Card";
-import './css/main.css';
+import "./css/main.css";
 
 function App() {
   const [cards, setCards] = useState([
@@ -24,7 +24,7 @@ function App() {
     {
       id: 3,
       imgSrc: null,
-      imgAlt: 'img alt',
+      imgAlt: "img alt",
       title: "Card title",
       text: "Some quick example text to build on the card title and make up the bulk of the card`s content.",
     },
@@ -41,51 +41,25 @@ function App() {
     <>
       <div className="card-container">
         {cards.map((card) => (
-          <Card
-            key={card.id}
-            imgSrc={card.imgSrc}
-            imgAlt={card.imgAlt}
-            title={card.title}
-            text={card.text}
-          ></Card>
+          <Card key={card.id}>
+            <img
+              src={card.imgSrc}
+              className="card-img-top"
+              alt={card.imgAlt}
+            />
+            <div className="card-body">
+              <h5 className="card-title">{card.title}</h5>
+              <p className="card-text">{card.text}</p>
+              <a
+                href="#"
+                className="btn btn-primary"
+              >
+                Go somewhere
+              </a>
+            </div>
+          </Card>
         ))}
       </div>
-      {/* <div>
-        <a
-          href="https://vitejs.dev"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={viteLogo}
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,31 +2,14 @@
 import "../css/main.css";
 import PropTypes from "prop-types";
 
-export const Card = ({imgSrc, imgAlt, title, text}) => {
+export const Card = (props) => {
   return (
     <div className="card">
-      <img
-        src={imgSrc}
-        className="card-img-top"
-        alt={imgAlt}
-      />
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{text}</p>
-        <a
-          href="#"
-          className="btn btn-primary"
-        >
-          Go somewhere
-        </a>
-      </div>
+      {props.children}
     </div>
   );
 };
 
 Card.propTypes = {
-  imgSrc: PropTypes.object,
-  imgAlt: PropTypes.string,
-  title: PropTypes.string,
-  text: PropTypes.string,
+  children: PropTypes.string,
 };
